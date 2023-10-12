9611209eec8fe109bf7a5ec56eb6677b0585e011
sakibhasan-24 <sakibhasan7724@gmail.com>
2023-10-13 02:33:14 +0600

    next and prev button complete

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const nextBtn = document.querySelector(".next");
+const prevBtn = document.querySelector(".prev");
 const imgContainer = document.querySelector(".image-container");
+const allImg = document.querySelectorAll(".image-container img");
+
 let currentImg = 1;
 nextBtn.addEventListener("click", function () {
   // next image will come
-
   currentImg++;
-  console.log(currentImg);
-  //   imgContainer.style.transform = `translateX(-${(currentImg - 1) * 55}rem)`;
+  if (currentImg > allImg.length) {
+    currentImg = 1;
+  }
+
+  imgContainer.style.transform = `translateX(-${(currentImg - 1) * 50}rem)`;
+});
+prevBtn.addEventListener("click", function () {
+  currentImg--;
+  if (currentImg === 1) {
+    currentImg = allImg.length;
+  }
+  imgContainer.style.transform = `translateX(-${(currentImg - 1) * 50}rem)`;
 });
